81a507411eb021df6ba0fd6372228d5b2fc81ce8
kamilpioro96 <48626405+kamilpioro96@users.noreply.github.com>
2019-03-17 12:14:45 +0100

    Wersja 1

---
+++ Grazaduzozamalo/Gramonolitycznie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gramonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj");
+            Console.Write("Podaj sowje imię:");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}!!!");
+            
+        }
+    }
+}
