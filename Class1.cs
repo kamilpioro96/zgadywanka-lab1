14fd8cda42ef165038ebafd32d9f0b802f0f7e46
kamilpioro96 <48626405+kamilpioro96@users.noreply.github.com>
2019-06-20 22:16:01 +0200

    Add files via upload
    Wersja 1 z blefem

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        void main()
+        {
+
+            int n = 8;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (j == n - 1)
+                    {
+                        Console.WriteLine("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n / 2 + 1; i++)
+            {
+                Console.WriteLine("*");
+            }
+            Console.ReadKey();
+        }
+	}
+}
